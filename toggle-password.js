abbba9615d73179b99fff6d51a9aa0ae48e57d57
Concept211 <pcarrau@gmail.com>
2024-04-18 10:06:49 -0400

    Create toggle-password.js

---
+++ toggle-password.js
@@ -0,0 +1,26 @@
+(function($) {
+    $.fn.togglePassword = function(options) {
+        var settings = $.extend({
+            show: 'fa-eye',
+            hide: 'fa-eye-slash',
+			base: 'fa fa-fw toggle-password-button'
+        }, options);
+
+        return this.each(function() {
+            var $input = $(this);
+            var $toggle = $('<span>')
+                .addClass(settings.base)
+                .addClass(settings.show)
+                .insertAfter($input)
+                .click(function() {
+                    $toggle.toggleClass(settings.show + ' ' + settings.hide);
+                    if ($input.attr('type') === 'password') {
+                        $input.attr('type', 'text');
+                    }
+					else {
+                        $input.attr('type', 'password');
+                    }
+                });
+        });
+    };
+})(jQuery);
